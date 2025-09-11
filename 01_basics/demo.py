a3507c5ec61424cf76580e78426ea518e4405927
vijaysanakattula <vijaysanakattula@gmail.com>
2025-09-11 07:05:10 +0530

    updated

--- 01_basics/demo.py
+++ 01_basics/demo.py
@@ -1,3 +1,20 @@
 print(9)
 hello = "hello hi"
 print(hello)
+
+a=10
+b=10
+print(a+b)
+
+# Identifiers
+data = "hello good morning"
+print(data)
+# 2day_data = "hello" # invalid as starts with digit 
+# print(2day_data)
+_data = "hello"
+print(_data)
+
+# Proper way of using identifiers
+student_name = "Ravi"
+student_name = 30
+student_enrolled_course = "python"
---
+++ 02_variables/demo.py
@@ -0,0 +1,35 @@
+# Working with Variables
+
+# Assigning Data
+student_name = "vijay"
+student_age = 20
+student_gpa = 9.5
+student_passed = True
+
+# Retreive Data
+print(student_name)
+print(student_age)
+print(student_gpa)
+print(student_passed)
+
+
+# Data
+print(id(student_name))
+print(id(student_age))
+print(id(student_gpa))
+print(id(student_passed))
+
+list_nums_a = [10,20,30,40,50]
+print(type(list_nums_a))
+print(id(list_nums_a))
+
+list_nums_b = [10,20,30,40,50]
+print(type(list_nums_b))
+print(id(list_nums_b))
+
+# To access data inside list we use index -> index starts from 0 1 2 ...
+# 0. 1. 2. 3. 4.
+# [10,20,30,40,50]
+print(list_nums_a) # print entire list
+print(list_nums_b) # print entire list
+
